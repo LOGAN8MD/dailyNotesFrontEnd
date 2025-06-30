a6a89864edd0a64b61cff990ddd386614079a2ac
Deepak Mishra <86044241+LOGAN8MD@users.noreply.github.com>
2025-06-30 20:30:37 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -10,7 +10,7 @@ const Signup = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const {name,email,password}=credentials;
-        const response = await fetch("https://dailynotesapi.herokuapp.com/api/auth/createuser", {
+        const response = await fetch("https://dailynotes-api-xj1d.onrender.com/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
