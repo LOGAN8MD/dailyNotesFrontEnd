eabe2957d71d7b80e8228bbdc36e166f2fe21b28
Deepak Mishra <86044241+LOGAN8MD@users.noreply.github.com>
2025-06-30 20:29:15 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,8 @@ import NoteContext from "./noteContext";
 
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
+  // const host = "http://localhost:5000"
+  const host="https://dailynotes-api-xj1d.onrender.com"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
